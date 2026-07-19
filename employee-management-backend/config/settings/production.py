d133266e3897acbeb6734a939b6db2849dab076d
Jitendra Doriya <jitendra662004@gmail.com>
2026-07-19 12:55:14 +0530

    Fix production settings

--- employee-management-backend/config/settings/production.py
+++ employee-management-backend/config/settings/production.py
@@ -1,7 +1,23 @@
+from .base import *
 import os
+
+DEBUG = False
+
+ALLOWED_HOSTS = os.environ.get(
+    "ALLOWED_HOSTS",
+    "localhost,127.0.0.1"
+).split(",")
 
 CSRF_TRUSTED_ORIGINS = [
     origin.strip()
     for origin in os.environ.get("CSRF_TRUSTED_ORIGINS", "").split(",")
     if origin.strip()
 ]
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = (
+    "whitenoise.storage.CompressedManifestStaticFilesStorage"
+)
