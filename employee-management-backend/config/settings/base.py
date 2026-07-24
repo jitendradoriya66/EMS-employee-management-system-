93c4746eb95498f5eedd35fa9859155288b00c37
Jitendra Doriya <jitendra662004@gmail.com>
2026-07-24 23:27:08 +0530

    fix: define ASGI_APPLICATION in Django base settings for routing configuration

--- employee-management-backend/config/settings/base.py
+++ employee-management-backend/config/settings/base.py
@@ -101,6 +101,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "config.wsgi.application"
+ASGI_APPLICATION = "config.asgi.application"
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
